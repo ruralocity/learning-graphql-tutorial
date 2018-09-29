c73cdb0f4359b8120bae639a116c161f997e738f
Aaron Sumner <asumner@oreilly.com>
2018-09-29 13:38:15 -0700

    Add many-to-many relationship between users and photos via tags

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type User {
@@ -23,6 +24,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   input PostPhotoInput {
@@ -70,6 +72,13 @@ var photos = [
   }
 ]
 
+var tags = [
+  { "photoID": "1", "userID": "gPlake" },
+  { "photoID": "2", "userID": "sSchmidt" },
+  { "photoID": "2", "userID": "mHattrup" },
+  { "photoID": "2", "userID": "gPlake" }
+]
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -91,13 +100,21 @@ const resolvers = {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
-    }
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
 
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
